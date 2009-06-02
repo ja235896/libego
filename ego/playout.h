cd9a3b6054e1a374028f77c3afd013bb3962c734
Jarek <ja235896@students.mimuw.edu.pl>
2009-06-02 16:08:35 +0200

    LocalPlayout distance increased

--- ego/playout.h
+++ ego/playout.h
@@ -5,6 +5,7 @@
 #include "board.h"
 
 #include <cmath>
+#include <iostream>
 
 // mercy rule
 
@@ -116,25 +117,57 @@ private:
     // No local move when game begins.
     if (board->move_no == 0) return false;
     // P(local) = 1/3
-    if (random.rand_int(3)) return false;
+    if (random.rand_int(100) >= 65) return false;
     
     //if (board->is_panic_mode()) return false;
 
     Vertex center = board->move_history[board->move_no-1].get_vertex();
 
-    Vertex legal[8];
-    legal[0] = center.NW();
-    legal[1] = center.N();
-    legal[2] = center.NE();
-    legal[3] = center.W();
-    legal[4] = center.E();
-    legal[5] = center.SW();
-    legal[6] = center.S();
-    legal[7] = center.SE();
+    Vertex legal[24];
+    //group A - dist 1 diag or horiz
+    legal[ 0] = center.N();
+    legal[ 1] = center.E();
+    legal[ 2] = center.S();
+    legal[ 3] = center.W();
 
+    //group B - dist 1
+    legal[ 4] = center.NE();
+    legal[ 5] = center.SE();
+    legal[ 6] = center.SW();
+    legal[ 7] = center.NW();
+
+    //group C - dist 2
+    legal[ 8] = center.NW().NW();
+    reps(i,  9, 12) legal[i] = legal[i-1].E();
+    legal[12] = center.NE().NE();
+    reps(i, 13, 16) legal[i] = legal[i-1].S();
+    legal[16] = center.SE().SE();
+    reps(i, 17, 20) legal[i] = legal[i-1].W();
+    legal[20] = center.SW().SW();
+    reps(i, 21, 24) legal[i] = legal[i-1].N();
+    
+    // (4/7 - A B C)
+    // (2/7 - B A C)
+    // (1/7 - C A B)
+    uint order = random.rand_int(7);
+    if (order == 4)
+      if (play_local_group(board, &legal[8], 16)) return true; //C
+    if (order >= 5)
+      if (play_local_group(board, &legal[4], 4)) return true; //B
+
+    if (play_local_group(board, &legal[0], 4)) return true; //A
+    if (order < 5)
+      if (play_local_group(board, &legal[4], 4)) return true; //B
+    if (order != 4)
+      if (play_local_group(board, &legal[8], 16)) return true; //C
+    
+    return false;
+  }
+
+  bool play_local_group(Board *board, Vertex* legal, uint legal_size) {
     Player act_player = board->act_player ();
 
-    uint i_start = random.rand_int(8), i = i_start;
+    uint i_start = random.rand_int(legal_size), i = i_start;
     do {
       if (legal[i].is_on_board() &&
           board->color_at[legal[i]] == Color::empty() &&
@@ -143,12 +176,13 @@ private:
         board->play_legal(act_player, legal[i]);
         return true;
       }
-      i = (i + 1) & 7;
+      i = (i + 1) & (legal_size - 1);
     } while (i != i_start);
 
     // No possibility of a local move
     return false;
   }
+
 
 };
 
